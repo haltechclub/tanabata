43f569e45e2744859317b4b6a82927fb806d7a76
Tsukasa Murata <stepfootprint@gmail.com>
2019-07-07 02:33:49 +0900

    一旦完成

---
+++ api/config.sample.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tsukasamurata
+ * Date: 2019/07/06
+ * Time: 20:27
+ */
+
+define('HOST', '');
+// MySQLのユーザー名
+define('USER', '');
+// MySQLのパスワード
+define('PASSWORD', '');
+// 接続するデータベース名
+define('DB_NAME', 'htc');
+
+define('AWS_ACCESS_KEY_ID', '');
+
+define('AWS_SECRET_ACCESS_KEY', '');
+
+define('AWS_SESSION_TOKEN', '');
+
+define('S3_BUCKET_NAME', '');
--- api/index.php
+++ api/index.php
@@ -6,7 +6,11 @@
  * Time: 20:26
  */
 
-require_once("config.php");
+use Aws\Credentials\CredentialProvider;
+use Aws\S3\S3Client;
+
+require_once "vendor/autoload.php";
+require_once "config.php" ;
 
 
 $link = mysqli_connect(HOST, USER, PASSWORD, DB_NAME);
@@ -39,23 +43,72 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 // レスポンス
     header("Content-Type: application/json; charset=utf-8");
     header('HTTP/1.1 200 OK');
-    echo json_encode($posts, JSON_UNESCAPED_UNICODE);
+    echo json_encode($posts);
 } else {
 
     // POST
     $id = $_POST["id"];
     $user_id = $_POST["user_id"];
-    $body = $_POST["body"];
-    $name = $_POST["name"];
+    $body = isset($_POST["body"]) ? $_POST["body"] : '';
+    $name = isset($_POST["name"]) ? $_POST["name"] : '';
     $color = $_POST["color"];
-    $sql = "insert into posts(id, user_id, body, name, color) VALUES ('$id', '$user_id', '$body', '$name', '$color');";
+    $background = null;
+
+    if ($_FILES["background"]["error"][0] == 0) {
+        $tmp_name = $_FILES["background"]["tmp_name"][0];
+        $ext = pathinfo($_FILES["background"]["name"][0], PATHINFO_EXTENSION);
+        $imgName = uniqid('', TRUE);
+
+        if (strtolower($ext) === 'png' || strtolower($ext) === 'jpg' || strtolower($ext) === 'gif') {
+            // ローカル保存
+            if (S3_BUCKET_NAME == '') {
+                move_uploaded_file($tmp_name, "data/$imgName." . $ext);
+                $background = "http://localhost/api/data/$imgName." . $ext;
+            } else {
+                // S3保存
+                putenv("AWS_ACCESS_KEY_ID=".AWS_ACCESS_KEY_ID);
+                putenv("AWS_SECRET_ACCESS_KEY=".AWS_SECRET_ACCESS_KEY);
+                putenv("AWS_SESSION_TOKEN=".AWS_SESSION_TOKEN);
+                $provider = CredentialProvider::env();
+                $s3 = new Aws\S3\S3Client([
+                    'version' => 'latest',
+                    'region' => 'us-east-1',
+                    'credentials' => $credentials
+                ]);
+                //バケット名を指定
+                $bucket = S3_BUCKET_NAME;
+                //アップロードするファイルを用意
+                $image = fopen($tmp_name,'rb');
+
+                //画像のアップロード
+                $result = $s3->putObject([
+                    'ACL' => 'public-read',
+                    'Bucket' => $bucket,
+                    'Key' => "$imgName." . $ext,
+                    'Body' => $image,
+                    'ContentType' => mime_content_type($tmp_name),
+                ]);
+                fclose($image);
+
+                //読み取り用のパスを返す
+                $background = $result['ObjectURL'];
+            }
+        }
+    }
+
+    $sql = "insert into posts(id, user_id, body, name, color, background) VALUES ('$id', '$user_id', '$body', '$name', '$color', '$background');";
     $res = mysqli_query($link, $sql);
 
-// DB切断
+    $sql = "select * from posts where id = '$id' order by created_at desc;";
+    $res = mysqli_query($link, $sql);
+    $posts = [];
+    while ($row = $res->fetch_assoc()) {
+        $posts[] = $row;
+    }
+
     mysqli_close($link);
 
-// レスポンス
     header("Content-Type: application/json; charset=utf-8");
     header('HTTP/1.1 201 Created');
-    echo json_encode([], JSON_UNESCAPED_UNICODE);
+    echo json_encode($posts[0]);
 }
